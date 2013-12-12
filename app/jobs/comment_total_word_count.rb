db7b9dd35380cc0b97541cb1ea438f612305c243
Nathaniel Watts <reg@nathanielwatts.com>
2013-12-11 20:56:50 -0700

    add redis DataCache class, and attach to the CommentTotalWordCount queue

--- app/jobs/comment_total_word_count.rb
+++ app/jobs/comment_total_word_count.rb
@@ -2,6 +2,6 @@ class CommentTotalWordCount
   @queue = :total_word_count
 
   def self.perform
-    Comment.total_word_count
+    DataCache.set 'comment_total_word_count', Comment.total_word_count
   end
 end
