c0b5775054ce04fb0e37d2c8540f531d1d5a0ad2
Nathaniel Watts <reg@nathanielwatts.com>
2013-12-11 21:14:46 -0700

    push the calculations back down to the model, now the controller has no idea of the implementation (which is what we want)

--- app/jobs/comment_total_word_count.rb
+++ app/jobs/comment_total_word_count.rb
@@ -2,6 +2,6 @@ class CommentTotalWordCount
   @queue = :total_word_count
 
   def self.perform
-    DataCache.set 'comment_total_word_count', Comment.total_word_count
+    Comment.calculate_total_word_count
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,11 +7,20 @@ class Comment < ActiveRecord::Base
     order('created_at DESC').limit(5).all
   end
 
+  def self.update_statistics
+    calculate_total_word_count
+  end
+
+  def self.calculate_total_word_count
+    total = all.inject(0) {|total, a| total += a.word_count }
+    DataCache.set 'comment/total_word_count', total
+  end
+
   def word_count
     body.split.count
   end
 
   def self.total_word_count
-    all.inject(0) {|total, a| total += a.word_count }
+    DataCache.get_i 'comment/total_word_count'
   end
 end
